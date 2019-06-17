d367605dad96a10b7e8c16e411305c892eebef61
Sean Towne <seanmichaeltowne@gmail.com>
2019-06-16 22:02:55 -0700

    deleted blablatest.txt

--- model/ColorMapper.java
+++ model/ColorMapper.java
@@ -7,10 +7,14 @@ import java.awt.Color;
 public class ColorMapper{
 	
 	public Color map(MandelbrotNumber number){
-		//mu = iterations + 1 - Math.Log(Math.Log(Z.Magnitude)) / log_escape;
+		/*//mu = iterations + 1 - Math.Log(Math.Log(Z.Magnitude)) / log_escape;
 		double mu = (
 			number.get_iteration_at_divergence() + 1 - Math.log(Math.log(number.get_magnitude_at_divergence())) / Math.log(AlgorithmContext.get_magnitude_cap())
-		);
+		);*/
+		if ( number.get_iteration_at_divergence() == AlgorithmContext.get_instance().get_iteration_cap()){
+			return Color.BLACK;
+		}
+		return Color.WHITE;
 	}
 
 	private static ColorMapper instance = null;
